16e790ba20622918290e11e681ed20fcbb8bff44
Darya Sushkova <dasha.030105@gmail.com>
2024-08-19 17:29:02 +0300

    added main panel to main page

---
+++ js/script.js
@@ -0,0 +1,10 @@
+new Swiper('.swiper', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    simulateTouch: false,
+    loop: true,
+    slidesPerView: 8,
+    spaceBetween: -50,
+});
